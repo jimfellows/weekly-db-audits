eec93ef9f126fa221a345e27f316108dcc327838
Jim Fellows <42395034+jimfellows@users.noreply.github.com>
2018-08-27 14:47:25 -0700

    Adding .py file of directory-related funcs.
    Uploading py file with functions related to file directories.  Currently a single function exists.

---
+++ dir_helpers.py
@@ -0,0 +1,14 @@
+import os
+
+
+def create_dir(filepath):
+    if os.path.exists(filepath):
+        print(filepath + ' already exists!')
+        return filepath
+    else:
+        os.makedirs(filepath)
+        print(filepath + ' folder created')
+
+
+
+
